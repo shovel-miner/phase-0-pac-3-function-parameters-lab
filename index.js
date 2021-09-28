be5bfd332cbdeb846bce94ea8bae49399132e0ed
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2021-09-28 12:51:36 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name = "Josh",language= "Ember.js"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+const  name = "Gracie"
+function introductionWithLanguageOptional(name,language ="JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
